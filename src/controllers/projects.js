5b92b84d5697875ff3808fec682fa832b696c806
AnyueWang <moonwong.way@gmail.com>
2024-06-19 18:26:17 +0200

    refactor: add default project after project list is created

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,11 +1,16 @@
-const projectList = []
+const project = require('../models/project')
+
+const defaultProject = project.createProject('Default')
+const projectList = [defaultProject]
 
 const showProjectList = () => projectList
 
 const addProject = (project) => projectList.push(project)
 
+const getDefaultProject = () => defaultProject
+
 module.exports = {
     showProjectList,
     addProject,
-    
+    getDefaultProject,
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const todo = require('./models/todo')
-const project = require('./models/project')
 const todos = require('./controllers/todos')
 const projects = require('./controllers/projects')
-
-const defaultProject = project.createProject('Default')
-
-projects.addProject(defaultProject)
 
 const itemTodo = todo.createTodo(
     'Shopping for food',
     new Date(2030, 5, 2),
-    defaultProject
+    projects.getDefaultProject(),
 )
 
 todos.addTodo(itemTodo)
